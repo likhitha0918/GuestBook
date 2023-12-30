7e96119e0f4eb3ba8fd13cf067c1623abb8f4f7b
Likhitha R <89694994+likhitha0918@users.noreply.github.com>
2023-12-30 21:15:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from flask 
-import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy 
-import SQLAlchemy
-from datetime 
-import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from google.cloud import ndb
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'  # Use your preferred DB connection
-db = SQLAlchemy(app)
+app.config['DEBUG'] = True
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    created = db.Column(db.DateTime, default=datetime.utcnow)
+# Set up Google Cloud NDB client
+client = ndb.Client()
+
+class Message(ndb.Model):
+    author = ndb.StringProperty()
+    content = ndb.TextProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
 
 @app.route('/')
 def index():
-    messages = Message.query.order_by(Message.created.desc()).all()
-    return render_template('index.html', messages=messages)
+    with client.context():
+        messages = Message.query().order(-Message.created).fetch()
+        return render_template('templates.html', messages=messages)
 
 @app.route('/sign', methods=['POST'])
 def sign():
-    author = request.form['author']
-    content = request.form['content']
+    author = request.form.get('author')
+    content = request.form.get('content')
 
     if author and content:
-        new_message = Message(author=author, content=content)
-        db.session.add(new_message)
-        db.session.commit()
+        with client.context():
+            message = Message(author=author, content=content)
+            message.put()
+        return redirect('/')
     else:
         error = "Please enter both your name and a message!"
-        return render_template('error.html', error=error)
-
-    return redirect(url_for('index'))
+        return render_template('templates.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
