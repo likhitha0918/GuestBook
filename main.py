dc3c1a331b3e8b049d62e9712125009b3e397ff5
Likhitha R <89694994+likhitha0918@users.noreply.github.com>
2023-12-30 21:18:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
-from google.cloud import ndb
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'  # Use any preferred DB connection
 
-# Set up Google Cloud NDB client
-client = ndb.Client()
+db = SQLAlchemy(app)
 
-class Message(ndb.Model):
-    author = ndb.StringProperty()
-    content = ndb.TextProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
-    with client.context():
-        messages = Message.query().order(-Message.created).fetch()
-        return render_template('templates.html', messages=messages)
+    messages = Message.query.order_by(Message.created.desc()).all()
+    return render_template('templates.html', messages=messages)
 
 @app.route('/sign', methods=['POST'])
 def sign():
@@ -25,13 +25,14 @@ def sign():
     content = request.form.get('content')
 
     if author and content:
-        with client.context():
-            message = Message(author=author, content=content)
-            message.put()
-        return redirect('/')
+        new_message = Message(author=author, content=content)
+        db.session.add(new_message)
+        db.session.commit()
+        return redirect(url_for('index'))
     else:
         error = "Please enter both your name and a message!"
         return render_template('templates.html', error=error)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
